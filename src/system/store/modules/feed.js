2b593414d7a1005c017fc2152b6e89100689691b
Park Hyun Seo <hyeonseo9669@hanmail.net>
2018-03-14 03:51:57 +0000

    Add action add_feed and remove_feed

--- src/system/store/modules/feed.js
+++ src/system/store/modules/feed.js
@@ -3,9 +3,13 @@ import { Record, List } from 'immutable';
 
 const CLICK_LIKE = 'feed/CLICK_LIKE';
 const WRITE_COMMENT = 'feed/WRITE_COMMENT';
+const ADD_FEED = 'feed/ADD_FEED';
+const REMOVE_FEED = 'feed/REMOVE_FEED';
 
 export const click_like = () => createAction(CLICK_LIKE);
 export const write_comment = () => createAction(WRITE_COMMENT);
+export const add_feed = () => createAction(ADD_FEED);
+export const remove_feed = () => createAction(REMOVE_FEED);
 
 let feed_id = 0; // feed 아이템에 들어갈 고유 값
 
@@ -22,7 +26,6 @@ const FeedRecord = Record({
     content: "",
     like: false,
     like_count: 0,
-    comment_id: 0,
     comments: List()
 })
 
@@ -41,8 +44,24 @@ export default handleActions({
   },
   // action 객체를 참조하지 않으니까 이렇게 생략을 할 수도 있겠죠?
   // state 부분에서 비구조화 할당도 해주어서 코드를 더욱 간소화시켰습니다.
-  [WRITE_COMMENT]: (state, { payload: comment }) => {
-    const item = CommentRecord({id: id++, author, text, })
+  [WRITE_COMMENT]: (state, action) => {
+    const { id, author, text } = action.payload;
+    const item = CommentRecord({id: id++, author, text});
     state.update('comments', comments => comments.push(item)) 
+  },
+  
+  // action 객체를 참조하지 않으니까 이렇게 생략을 할 수도 있겠죠?
+  // state 부분에서 비구조화 할당도 해주어서 코드를 더욱 간소화시켰습니다.
+  [ADD_FEED]: (state, action) => {
+    const { id, author, title, content } = action.payload;
+    const feed = FeedRecord({id: id++, author, title, content});
+    state.update('feeds', feeds => feeds.push(feed)) 
+  },
+  
+  // action 객체를 참조하지 않으니까 이렇게 생략을 할 수도 있겠죠?
+  // state 부분에서 비구조화 할당도 해주어서 코드를 더욱 간소화시켰습니다.
+  [REMOVE_FEED]: (state, {payload: id}) => {
+    const index = state.get('feeds'.findIndex(item => item.get('id') === id));
+    state.deleteIn(['feeds', index])
   }
 }, initialState);

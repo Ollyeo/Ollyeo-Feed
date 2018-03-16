55d533a0b046bece1c55c4abe1aa68b808e4d970
Park Hyun Seo <hyeonseo9669@hanmail.net>
2018-03-16 03:57:06 +0000

    Add delete comment action

--- src/system/store/modules/feed.js
+++ src/system/store/modules/feed.js
@@ -1,26 +1,50 @@
+// react-logger, redux-thunk, redux-pender
+
 import { createAction, handleActions } from 'redux-actions';
-import { Record, List } from 'immutable';
+import { Map, List } from 'immutable';
 
 const CLICK_LIKE = 'feed/CLICK_LIKE';
-const WRITE_COMMENT = 'feed/WRITE_COMMENT';
 const ADD_FEED = 'feed/ADD_FEED';
-const REMOVE_FEED = 'feed/REMOVE_FEED';
+const DELETE_FEED = 'feed/DELETE_FEED';
+
+const CHANGE_COMMENT = 'comment/CHANGE_COMMENT';
+const DELETE_COMMENT = 'comment/DELETE_COMMENT';
+const WRITE_COMMENT = 'comment/WRITE_COMMENT';
+
+/*
+이런식으로 하면 그 파라미터의 값이 index가 될 지 뭐가 될 지 모른다.
+그렇기 때문에, 파라미터로 전달받은 값을 액션의 payload 값으로 설정해준다.
+따라서 click_like가 실행된다면
+{
+  type:'feed/CLICK_LIKE',
+  payload:1
+}
+
+*/
+
+/*
+{
+  type:'feed/CLICK_LIKE',
+  payload:(index)
+}
+*/
+export const change_comment = () => createAction(CHANGE_COMMENT);
+export const write_comment = () => createAction(WRITE_COMMENT);
+export const delete_comment = () => createAction(DELETE_COMMENT)
 
 export const click_like = () => createAction(CLICK_LIKE);
-export const write_comment = () => createAction(WRITE_COMMENT);
 export const add_feed = () => createAction(ADD_FEED);
-export const remove_feed = () => createAction(REMOVE_FEED);
-
-let feed_id = 0; // feed 아이템에 들어갈 고유 값
+export const remove_feed = () => createAction(DELETE_FEED);
 
 // 모듈의 초기 상태를 정의합니다.
 const initialState = {
+  feed_id: 0,
   comment_input:'',
   feeds: List()
 };
 
-const FeedRecord = Record({
-    id: feed_id++,
+const FeedRecord = Map({
+    id: initialState.feed_id++,
     author: "",
     title: "",
     content: "",
@@ -29,7 +53,7 @@ const FeedRecord = Record({
     comments: List()
 })
 
-const CommentRecord = Record({
+const CommentRecord = Map({
   id: 0,
   author: "",
   comment: "",
@@ -38,30 +62,44 @@ const CommentRecord = Record({
 
 export default handleActions({
   [CLICK_LIKE]: (state, action) => {
-    return { 
-      like_count: state.update('like_count' ,  value => value+ 1) 
-    };
-  },
-  // action 객체를 참조하지 않으니까 이렇게 생략을 할 수도 있겠죠?
-  // state 부분에서 비구조화 할당도 해주어서 코드를 더욱 간소화시켰습니다.
-  [WRITE_COMMENT]: (state, action) => {
-    const { id, author, text } = action.payload;
-    const item = CommentRecord({id: id++, author, text});
-    state.update('comments', comments => comments.push(item)) 
+    const index = action.payload;
+    const feed = state.get('feeds', index);
+    
+    feed.update('like_count',  value => feed.like ? value - 1: value + 1),
+    feed.update('like', value => !feed.like);
   },
   
-  // action 객체를 참조하지 않으니까 이렇게 생략을 할 수도 있겠죠?
-  // state 부분에서 비구조화 할당도 해주어서 코드를 더욱 간소화시켰습니다.
   [ADD_FEED]: (state, action) => {
     const { id, author, title, content } = action.payload;
     const feed = FeedRecord({id: id++, author, title, content});
+    
     state.update('feeds', feeds => feeds.push(feed)) 
+  },
+  
+  [DELETE_FEED]: (state, {payload: id}) => {
+    const index = state.get('feeds').findIndex(item => item.get('id') === id);
+    
+    state.deleteIn(['feeds', index])
   },
   
   // action 객체를 참조하지 않으니까 이렇게 생략을 할 수도 있겠죠?
   // state 부분에서 비구조화 할당도 해주어서 코드를 더욱 간소화시켰습니다.
-  [REMOVE_FEED]: (state, {payload: id}) => {
-    const index = state.get('feeds'.findIndex(item => item.get('id') === id));
-    state.deleteIn(['feeds', index])
-  }
+  [CHANGE_COMMENT]: (state, action) => state.set('comment_input', action.payload),
+  
+  // TODO:: Feed의 아이디를 찾아서 Comment 대입
+  [WRITE_COMMENT]: (state, action) => {
+    const { feed_id, author, text } = action.payload;
+    const item = CommentRecord({id: feed_id.comments.length()+1, author, text});
+    
+    state.updateIn(['feeds', feed_id, 'comments'], comments => comments.push(item)) 
+  },
+  
+  // TODO:: Feed의 아이디를 찾아서 Comment 대입
+  [DELETE_COMMENT]: (state, action) => {
+    const fi = state.get('feeds').findIndex(item => item.get('feed_id') === action.payload.feed_id).fromJS()
+    const ci = state.getIn(['feeds', fi, 'comments']).findIndex(item => item.get('comment_id') === action.payload.comment_id).fromJS()
+    
+    state.deleteIn(['feeds', fi, 'comments', ci]);
+  },
+
 }, initialState);
